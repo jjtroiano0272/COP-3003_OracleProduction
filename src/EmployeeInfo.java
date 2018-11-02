ff7db606a15e60558f1b5546c0f5e22749aee793
jjtroiano0272 <jjtroiano0272@eagle.fgcu.edu>
2018-11-02 11:14:39 -0400

    Initial commit. Worked on Step 18 today. FRI 02 NOV

---
+++ src/EmployeeInfo.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class EmployeeInfo {
+	
+	private StringBuilder name
+	private String code;
+	
+	public StringBuilder getName()
+	
+	public String getcode(){;}
+	
+	//We know
+	private void setNam(){
+		String nameString = inputName();
+		name = new StringBuilder(nameString) // one of the constructor for namebuiolder accepts a string(nameString here)
+		createEmployeeCode(StringBuilder name)
+	}
+	
+	// Here we set the code.
+	private void createEmployeeCode(StringBuilder name){
+		if (checkName(name)) { // Indication of validity
+		
+		}
+		if (name.indexOf()) {
+			code = name.charAt(0)+name.substring(name.indexOf(" "+1))
+		}
+		else {
+			code =
+		}
+	}
+	
+	//
+	private String inputName(){
+		Scanner in = new Scanner(System.in);
+		System.out.println("Print name:");
+		StringBuilder nameString = in.nextLine();
+		return nameString;
+	}
+	
+	private
+	
+	boolean checkName(StringBuilder name) {
+		if (name.indexOf())
+	}
+}
